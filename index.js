d6c2c258b4918012466acc225249e360e199a973
Saiful Irham Wicaksana <icaksama@gmail.com>
2018-06-03 15:46:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(
 function getCookie(cname){var name=cname+"=";var decodedCookie=decodeURIComponent(document.cookie);var ca=decodedCookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)===' '){c=c.substring(1)}
 if(c.indexOf(name)===0){return c.substring(name.length,c.length)}}
 return""}
+function setCookie(cname,cvalue,exdays){var d=new Date();d.setTime(d.getTime()+(exdays*24*60*60*1000));var expires="expires="+d.toUTCString();document.cookie=cname+"="+cvalue+";"+expires+";path=/"}
+function getCookie(cname){var name=cname+"=";var decodedCookie=decodeURIComponent(document.cookie);var ca=decodedCookie.split(';');for(var i=0;i<ca.length;i++){var c=ca[i];while(c.charAt(0)===' '){c=c.substring(1)}
+if(c.indexOf(name)===0){return c.substring(name.length,c.length)}}
+return""}
 function isCookieExist(cname){var username=getCookie(cname);if(username!=""){return!0}else{return!1}}
 jQuery(document).ready(function(){var clickCount=0;if(isCookieExist(idAdsense)){clickCount=parseInt(getCookie(idAdsense),10)}else{setCookie(idAdsense,"0",1)}
-if(clickCount>=limitClick){jQuery("div[id="+idAdsense+"]").remove()}else{jQuery('#'+idAdsense+' iframe').iframeTracker({blurCallback:function(event){if(this._overId==idAdsense){clickCount++;setCookie(idAdsense,""+clickCount+"",1);setTimeout(function(){jQuery("div[id="+idAdsense+"]").remove()},250)}},overCallback:function(element,event){this._overId=jQuery(element).parents('#'+idAdsense).attr('id')},outCallback:function(element,event){this._overId=null},_overId:null})}})
+if(clickCount>=limitClick){jQuery("div[id="+idAdsense+"]").remove()}else{jQuery('#'+idAdsense+' iframe').iframeTracker({blurCallback:function(event){clickCount++;setCookie(idAdsense,""+clickCount+"",1);setTimeout(function(){jQuery("div[id="+idAdsense+"]").remove()},250)},overCallback:function(element,event){this._overId=jQuery(element).parents('#'+idAdsense).attr('id')},outCallback:function(element,event){},})}})
